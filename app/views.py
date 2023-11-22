d72ca7d673648008a4d0c9c19dc1921e8f63b059
Suraj kushwaha <skk74143@gmail.com>
2023-11-22 22:57:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sudheer(request):
+    return render(request,'sudheer.html')
+def sujana(request):
+    return render(request,'sujana.html')    
